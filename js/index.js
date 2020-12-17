c2e0cc243b028e936a4009a658919b52beeea793
namsefroehlich <66533679+namsefroehlich@users.noreply.github.com>
2020-12-17 23:11:01 +0100

    Landing Page Fades

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 
+    // Landing Page Fade In's
     $(".header").fadeTo(2000, 1);
     $("#full-name").fadeTo(2500, 1);
     $("#job-title").fadeTo(3000, 1);
     $("#blue-line").fadeTo(3000, 1);
+    $(".arrow-wrapper").fadeTo(3000, 1);
 });
