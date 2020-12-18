52b73c99172c50ebbbfb9aa5d0f950665e9b6f05
namsefroehlich <66533679+namsefroehlich@users.noreply.github.com>
2020-12-18 04:53:44 +0100

    Various Changes + Logo + CNAME + HoverTransitions

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
     // Landing Page Fade In's
-    $(".header").fadeTo(2000, 1);
-    $("#full-name").fadeTo(2500, 1);
-    $("#job-title").fadeTo(3000, 1);
-    $("#blue-line").fadeTo(3000, 1);
+    $(".header").fadeTo(1000, 1);
+    $("#full-name").fadeTo(500, 1);
+    $("#job-title").fadeTo(1000, 1);
+    $("#blue-line").fadeTo(1000, 1);
     $(".arrow-wrapper").fadeTo(3000, 1);
 });
